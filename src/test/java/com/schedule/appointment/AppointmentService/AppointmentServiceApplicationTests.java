1ae960000b5d2172c007a2e93ffb7921a1a89a7a
Satya Akhil Pavan Kumar Reddy Padala <p.akhilreddy1289@gmail.com>
2023-07-08 14:37:33 -0500

    Initial commit

---
+++ src/test/java/com/schedule/appointment/AppointmentService/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule.appointment.AppointmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
